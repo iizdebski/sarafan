5782de27504102bbc5b48eeabb0a1ea9addc4a5f
Ivan <iizdepskyy112@gmail.com>
2021-03-31 23:08:51 +0300

    4_db_jackson_REST

---
+++ src/main/java/com/letscode/sarafan/repo/MessageRepo.java
@@ -0,0 +1,8 @@
+package com.letscode.sarafan.repo;
+
+import com.letscode.sarafan.domain.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessageRepo extends JpaRepository<Message, Long> {
+
+}
